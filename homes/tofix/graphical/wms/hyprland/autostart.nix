8404ea442d70f7667b0149cc07b61943fd50b794
tofixrs <tofix.rs@proton.me>
2024-02-04 21:53:42 +0100

    feat: QOL stuff

--- homes/tofix/graphical/wms/hyprland/autostart.nix
+++ homes/tofix/graphical/wms/hyprland/autostart.nix
@@ -1,8 +1,8 @@
-{pkgs}: [
+_: [
   "hyprctl setcursor Bibata-Modern-Classic 12"
   "wl-paste --type text --watch cliphist store"
   "wl-paste --type image --watch cliphist store"
   # Possiby turn this into a wayland service
   "ags"
-  "sleep 5 && swww init && swww img /home/tofix/wallpaper.jpg"
+  "sleep 1 && swww init && swww img /home/tofix/wallpaper.jpg"
 ]
--- homes/tofix/other-apps.nix
+++ homes/tofix/other-apps.nix
@@ -35,5 +35,6 @@
     libreoffice
     qbittorrent
     gimp
+    fastfetch
   ];
 }
