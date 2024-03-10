1701c76126c35a3fdf8a0d574da241ea1920755b
tofixrs <tofix.rs@proton.me>
2024-03-10 11:37:59 +0100

    jsonls

--- homes/tofix/terminal/editors/nvim/lsp.nix
+++ homes/tofix/terminal/editors/nvim/lsp.nix
@@ -27,35 +27,44 @@ in {
       lspkind.enable = true;
       lightbulb.enable = true;
       trouble.enable = true;
-      lspconfig.sources.csharp_ls = ''
-        lspconfig.csharp_ls.setup {
-          capabilities = capabilities;
-          on_attach = default_on_attach;
-          cmd = {"${getExe pkgs.csharp-ls}"};
-        }
-      '';
-      lspconfig.sources.lua-lsp = ''
-        lspconfig.lua_ls.setup {
-          capabilities = capabilities;
-          on_attach = default_on_attach;
-          cmd = {"${getExe pkgs.lua-language-server}"};
-
-          settings  = {
-          	Lua = {
-          		workspace = {
-          			library = {
-          				vim.fn.expand("$HOME/sm.lua")
-          			}
-          		}
-          	}
+      lspconfig.sources = {
+        jsonls = ''
+          lspconfig.jsonls.setup {
+            capabilities = capabilities;
+            on_attach = default_on_attach;
           }
-        }
-      '';
+        '';
+        csharp_ls = ''
+          lspconfig.csharp_ls.setup {
+            capabilities = capabilities;
+            on_attach = default_on_attach;
+            cmd = {"${getExe pkgs.csharp-ls}"};
+          }
+        '';
+        lua-lsp = ''
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities;
+            on_attach = default_on_attach;
+            cmd = {"${getExe pkgs.lua-language-server}"};
+
+            settings  = {
+            	Lua = {
+            		workspace = {
+            			library = {
+            				vim.fn.expand("$HOME/sm.lua")
+            			}
+            		}
+            	}
+            }
+          }
+        '';
+      };
     };
   };
 
   home.packages = with pkgs; [
     csharp-ls
     dotnet-sdk_8
+    vscode-langservers-extracted
   ];
 }
