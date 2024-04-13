ce7ba7fa88d3dd91fff6d97c2fcecb0a1da4fa1f
tofixrs <tofix.rs@proton.me>
2024-04-13 11:11:16 +0200

    fix: switch from colorscheme.colors to colorscheme.palette

--- homes/tofix/services/wayland/swaylock.nix
+++ homes/tofix/services/wayland/swaylock.nix
@@ -4,38 +4,38 @@
   ...
 }: {
   programs.swaylock = let
-    inherit (config.colorscheme) colors;
+    inherit (config.colorscheme) palette;
   in {
     enable = true;
     package = with pkgs; swaylock-effects;
     settings = {
       clock = true;
-      color = "${colors.base00}";
+      color = "${palette.base00}";
       font = "JetBrainsMono Nerd Font";
       show-failed-attempts = true;
       indicator = true;
       indicator-radius = 200;
       indicator-thickness = 20;
-      line-color = "${colors.base00}";
-      ring-color = "${colors.base04}";
-      inside-color = "${colors.base00}";
-      key-hl-color = "${colors.base0F}";
+      line-color = "${palette.base00}";
+      ring-color = "${palette.base04}";
+      inside-color = "${palette.base00}";
+      key-hl-color = "${palette.base0F}";
       separator-color = "00000000";
-      text-color = "${colors.base05}";
+      text-color = "${palette.base05}";
       text-caps-lock-color = "";
-      line-ver-color = "${colors.base0F}";
-      ring-ver-color = "${colors.base0F}";
-      inside-ver-color = "${colors.base00}";
-      text-ver-color = "${colors.base05}";
-      ring-wrong-color = "${colors.base08}";
-      text-wrong-color = "${colors.base08}";
-      inside-wrong-color = "${colors.base00}";
-      inside-clear-color = "${colors.base00}";
-      text-clear-color = "${colors.base05}";
-      ring-clear-color = "${colors.base0B}";
-      line-clear-color = "${colors.base00}";
-      line-wrong-color = "${colors.base00}";
-      bs-hl-color = "${colors.base08}";
+      line-ver-color = "${palette.base0F}";
+      ring-ver-color = "${palette.base0F}";
+      inside-ver-color = "${palette.base00}";
+      text-ver-color = "${palette.base05}";
+      ring-wrong-color = "${palette.base08}";
+      text-wrong-color = "${palette.base08}";
+      inside-wrong-color = "${palette.base00}";
+      inside-clear-color = "${palette.base00}";
+      text-clear-color = "${palette.base05}";
+      ring-clear-color = "${palette.base0B}";
+      line-clear-color = "${palette.base00}";
+      line-wrong-color = "${palette.base00}";
+      bs-hl-color = "${palette.base08}";
       line-uses-ring = false;
       datestr = "%d/%m/%Y";
       fade-in = "0.1";
