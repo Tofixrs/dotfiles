f5867b002fe39750b087f1bb5e86585103236f8b
tofixrs <tofix.rs@proton.me>
2024-07-04 14:52:50 +0200

    chore(nix): update

--- homes/tofix/graphical/apps/spotify.nix
+++ homes/tofix/graphical/apps/spotify.nix
@@ -4,6 +4,16 @@
   ...
 }: let
   spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+  beautiful-lyrics-src = pkgs.fetchFromGitHub {
+    owner = "surfbryce";
+    repo = "beautiful-lyrics";
+    rev = "3f4cfdc05c8217d37f041cc09f30376c2af27612";
+    hash = "sha256-K01zSEL/t8RxbDdWI/2oGaZh0Wi5fbJXyIPAIh17GJ4=";
+  };
+  beautiful-lyrics = {
+    src = "${beautiful-lyrics-src}/Builds/Release";
+    filename = "beautiful-lyrics.mjs";
+  };
 in {
   imports = [inputs.spicetify-nix.homeManagerModule];
 
@@ -22,6 +32,7 @@ in {
       fullAlbumDate
       groupSession
       popupLyrics
+      beautiful-lyrics
     ];
   };
 }
