60e852d5a955f0c2c15552aa26a1db06d39f8461
tofixrs <tofix.rs@proton.me>
2024-07-08 12:44:36 +0200

    feat: more tweaks to sway idle\

--- homes/tofix/services/wayland/hypridle.nix
+++ homes/tofix/services/wayland/hypridle.nix
@@ -5,18 +5,18 @@ _: {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
         before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        # Sleep 3 seconds cuz gotta wait for system to properly do what it needs to
+        after_sleep_cmd = "sleep 3 && hyprctl dispatch dpms on";
       };
       listener = [
         {
-          timeout = 180;
+          timeout = 120;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
         {
-          timeout = 120;
+          timeout = 180;
           on-timeout = "loginctl lock-session";
-          on-resume = "hyprctl dispatch dpms on";
         }
         {
           timeout = 300;
