56810fb03c61c67b39da50f82387c79c69d0f686
tofixrs <tofix.rs@proton.me>
2024-07-12 23:20:31 +0200

    feat: util shell commands

--- parts/shell.nix
+++ parts/shell.nix
@@ -11,6 +11,15 @@
           name = "switch";
           text = ''sudo nixos-rebuild switch --flake "git+file://$(pwd)?submodules=1#lapfix" --show-trace'';
         })
+        (pkgs.writeShellApplication {
+          name = "clean";
+          text = ''sudo nix-collect-garbage -d && nix-collect-garbage -d'';
+        })
+        (pkgs.writeShellApplication {
+          name = "print-roots";
+          text = ''nix-store --gc --print-roots | grep -E "$(hostname)|home-manager" | column -t | sort -k3 -k1'';
+        })
+
         inputs'.agenix.packages.default
       ];
     };
