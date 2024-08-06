3765a96d006340616e66603542634c168a7a7b36
tofixrs <tofix.rs@proton.me>
2024-08-06 19:25:53 +0200

    feat: temp switch to normal firefox due to crashes

--- homes/tofix/graphical/apps/firefox/default.nix
+++ homes/tofix/graphical/apps/firefox/default.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: let
-  profile-name = "dev-edition-default";
+  profile-name = "default";
 in {
   programs.firefox = {
     enable = true;
     profiles."${profile-name}" = {
       extraConfig = (import ./betterfoxjs.nix {}) + (import ./userjs.nix {});
     };
-    package = pkgs.firefox-devedition;
+    package = pkgs.firefox;
     nativeMessagingHosts = [pkgs.kdePackages.plasma-browser-integration];
   };
   home.file.".mozilla/firefox/${profile-name}/chrome".source = ./ArcWTF;
