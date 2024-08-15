fd93b048eed9bba0e969f87d99522b0a676953dc
tofixrs <tofix.rs@proton.me>
2024-08-16 00:16:00 +0200

    feat: wakeup from dpms upon mouse or key press

--- homes/tofix/graphical/wms/hyprland/default.nix
+++ homes/tofix/graphical/wms/hyprland/default.nix
@@ -80,6 +80,8 @@ in {
         misc = {
           disable_hyprland_logo = true;
           vrr = 1;
+          mouse_move_enables_dpms = true;
+          key_press_enables_dpms = true;
         };
         debug = {
           disable_logs = false;
