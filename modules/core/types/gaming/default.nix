dcc74d41386fdb37287ce168257ac2285bd3b6f5
tofixrs <tofix.rs@proton.me>
2024-08-15 16:39:15 +0200

    feat: add steam tinker launch

--- modules/core/types/gaming/default.nix
+++ modules/core/types/gaming/default.nix
@@ -1,7 +1,10 @@
-_: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
+  environment.systemPackages = with pkgs; [
+    steamtinkerlaunch
+  ];
 }
