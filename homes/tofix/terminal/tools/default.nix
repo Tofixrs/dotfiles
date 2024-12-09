dce2b67fba049679a86aa0ef417088cc590a64ee
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2024-12-09 21:43:17 +0100

    setup git commit signing
    hopefully didnt expose anythin lol

--- homes/tofix/terminal/tools/default.nix
+++ homes/tofix/terminal/tools/default.nix
@@ -1,3 +1,3 @@
 _: {
-  imports = [./fastfetch.nix];
+  imports = [./fastfetch.nix ./git.nix];
 }
---
+++ homes/tofix/terminal/tools/git.nix
@@ -0,0 +1,11 @@
+_: {
+  programs.git = {
+    enable = true;
+    userName = "Tofixrs";
+    userEmail = "73693639+Tofixrs@users.noreply.github.com";
+    signing = {
+      key = "89BC99F91C0D511E";
+      signByDefault = true;
+    };
+  };
+}
