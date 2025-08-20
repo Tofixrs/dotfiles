97861e60d0ae8f9e9d30678e123f33ea831c0bfb
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2025-08-20 12:54:30 +0200

    fix: qt

--- homes/tofix/themes/qt.nix
+++ homes/tofix/themes/qt.nix
@@ -1,11 +1,13 @@
 {pkgs, ...}: {
-  qt.enable = true;
+  qt = {
+    enable = true;
+    platformTheme = "qtct";
+  };
   home = {
     packages = with pkgs; [
       libsForQt5.qtstyleplugin-kvantum
       #nixpkgs for fuck sake PLEASE ass a naming convention for you packages why isnt this libForQt6.[...] but fucking kdePackages.[...]
       kdePackages.qtstyleplugin-kvantum
     ];
-    sessionVariables = {QT_STYLE_OVERRIDE = "kvantum";};
   };
 }
