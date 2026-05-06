d33bd26ee3748162a53db5e351ccbd80c1397bd0
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2026-05-06 23:34:42 +0200

    reenable hardware cursors cuz lag

--- homes/tofix/graphical/wms/hyprland/default.nix
+++ homes/tofix/graphical/wms/hyprland/default.nix
@@ -91,7 +91,7 @@ in {
             disable_logs = false;
           };
           cursor = {
-            no_hardware_cursors = false;
+            no_hardware_cursors = true;
           };
           gesture = [
             "3, horizontal, workspace"
