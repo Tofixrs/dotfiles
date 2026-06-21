adaddbeffb6b1236bc05d85e7775e6f9d6e5e82d
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2026-06-21 22:02:50 +0200

    fix pls

--- modules/nixos/system/boot/generic.nix
+++ modules/nixos/system/boot/generic.nix
@@ -14,7 +14,7 @@ in {
       kernelPackages = sys.boot.kernel;
       loader.efi.canTouchEfiVariables = true;
       kernelModules = ["v4l2loopback"];
-      extraModulePackages = mkDefault (sys.boot.extraModulePackages ++ [config.boot.kernelPackages.v4l2loopback]);
+      extraModulePackages = sys.boot.extraModulePackages ++ [config.boot.kernelPackages.v4l2loopback];
       extraModprobeConfig = ''
         options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
       '';
