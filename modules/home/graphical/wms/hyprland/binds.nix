969eca5955334354bd69537e89d839c91f8da13e
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2026-07-29 18:42:15 +0200

    mute with 250ms delay

--- modules/home/graphical/wms/hyprland/binds.nix
+++ modules/home/graphical/wms/hyprland/binds.nix
@@ -244,7 +244,7 @@ in {
       })
       (mkBind {
         key = "F10";
-        dsp = "hl.dsp.exec_cmd(\"wpctl set-mute @DEFAULT_SOURCE@ 1\")";
+        dsp = "hl.timer(function() hl.dsp.exec_cmd(\"wpctl set-mute @DEFAULT_SOURCE@ 1\") end, {timeout = 250, type = \"oneshot\"})";
         flags = {release = true;};
       })
       (mkBind {
